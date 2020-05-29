72c8264b4274cb18c97b9afce9fb19ac247aca59
Bhavit kanthalia <bhavitkanthalia16@gmail.com>
2020-05-29 11:55:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/schedulertodo/db/NoteDao.kt
@@ -0,0 +1,4 @@
+package com.example.schedulertodo.db
+
+interface NoteDao {
+}
---
+++ app/src/main/java/com/example/schedulertodo/ui/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.schedulertodo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.schedulertodo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navController = Navigation.findNavController(this,R.id.fragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(this,R.id.fragment),
+            null
+        )
+    }
+}
